a0000d42ac58bd5e7f5277d99cfdbc2fdb20d916
LeonGamerPS1 <fnfmaker666@gmail.com>
2024-09-13 22:57:59 +0200

    INITIAL COMMIT

---
+++ source/funkin/PlayState.hx
@@ -0,0 +1,14 @@
+package funkin;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
---
+++ source/funkin/objects/StrumLine.hx
@@ -0,0 +1,9 @@
+package funkin.objects;
+
+class StrumLine extends FlxSprite
+{
+	public function new(x, y, player:Int = 1, lanes:Int = 4)
+	{
+		super(x, y);
+	}
+}
